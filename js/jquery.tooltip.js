1051422b71e2ed043fdc8adcc93c46604e4d18ac
jahusayuri <josh.yuri.requioma@gmail.com>
2018-02-24 00:10:02 +0800

    Added basic files; Still need to fix mapping in the images

---
+++ js/jquery.tooltip.js
@@ -0,0 +1,11 @@
+
+/* For Nervous System Tooltip */
+
+//initializing tooltip
+$("area").tooltip({
+    track: true
+});
+
+$("area#frontal_lobe").tooltip({
+    position: {at: "left-275 top-375", of: "area"}
+});
